2ac3149a8972e049b6f2543c15339173e706941b
nirajsbajaj <bajajsniraj@gmail.com>
2026-09-07 20:16:27 +0530

    update nvim config

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -11,6 +11,13 @@ return {
             nerd_font_variant = "mono",
         },
 
+        completion = {
+            documentation = {
+                auto_show = true,
+                auto_show_delay_ms = 200,
+            },
+        },
+
         sources = {
             default = {"lsp", "path", "buffer"},
         },
